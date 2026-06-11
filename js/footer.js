f761295c4dc51290938dfd908e7a32923ea29d0d
iamwairimu <kmnleah@gmail.com>
2026-06-11 15:11:38 +0300

    service page done

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,7 @@
+// Fetch footer content
 fetch("footer.html")
   .then((response) => response.text())
   .then((data) => {
+    // Insert footer content into the container
     document.getElementById("footer-container").innerHTML = data;
   });
